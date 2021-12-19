b505a41ead727c83a11c45c5b478424763b6a27f
Carlos Toxtli <ctoxtli@gmail.com>
2021-12-19 13:52:25 -0500

    Update data_helpers.py

--- code/data_helpers.py
+++ code/data_helpers.py
@@ -333,7 +333,15 @@ def build_input_data(sentences, labels, vocabulary):
     """
     Maps sentences and labels to vectors based on a vocabulary.
     """
-    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
+    x = []
+    for sentence in sentences:
+        record = []
+        for word in sentence:
+            print(word)
+            print(vocabulary[word])
+            record.append(vocabulary[word])
+        x.append(record)
+    # x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
     y = np.array(labels)
     return [x, y]
 
